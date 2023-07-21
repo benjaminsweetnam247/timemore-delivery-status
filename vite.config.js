5dc9ee017606d0f6e2a60e7a4d0636aef1fe5e23
Ben Sweetnam <5063792+benjaminsweetnam247@users.noreply.github.com>
2023-07-21 22:04:45 +1000

    changing title

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/'
+  base: '/timemore-delivery-status/',
   plugins: [
     vue(),
   ],
